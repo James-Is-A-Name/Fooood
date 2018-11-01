ecef324b13776f3ff6a47e804d47caff33d64ff4
Arry <Arry@Arry.arr>
2018-11-02 11:14:17 +1300

    all dishes show with ingredient

--- db.js
+++ db.js
@@ -1,18 +1,50 @@
-const environment = process.env.NODE_ENV || 'development'
-const config = require('./knexfile')[environment]
-const connection = require('knex')(config)
+const environment = process.env.NODE_ENV || "development";
+const config = require("./knexfile")[environment];
+const connection = require("knex")(config);
 
 module.exports = {
-  getUser: getUser,
-  getUsers: getUsers
+  getIngredients: getIngredients,
+  getAllDishes: getAllDishes
+};
+
+function getIngredients(testConn) {
+  const conn = testConn || connection;
+  return conn("ingredients").select();
 }
 
-function getUsers (testConn) {
-  const conn = testConn || connection
-  return conn('users').select()
+function getAllDishes(testConn) {
+  const conn = testConn || connection;
+  return conn("dishes")
+    .join("recipe_joins", "dishes.id", "recipe_joins.dish_id")
+    .select("*", "dishes.name AS dish_name")
+    .join("ingredients", "ingredients.id", "recipe_joins.ingredient_id")
+    .then(dishes => {
+      return formatDishes(dishes);
+    });
 }
 
-function getUser (id, testConn) {
-  const conn = testConn || connection
-  return conn('users').where('id', id).first()
+function formatDishes(dishes) {
+  let dishesList = [];
+  for (let i = 0; i < dishes.length; i++) {
+    if (!dishesList.includes(dishes[i].dish_name)) {
+      dishesList.push(dishes[i].dish_name);
+    }
+  }
+
+  //create list of dish objects
+  dishesList = dishesList.map(dish => {
+    return { name: dish, items: [] };
+  });
+
+  //Check ingredients and add to dish if has dish name
+  for (let i = 0; i < dishes.length; i++) {
+    //finds dishlist item that matches ingredient dish name
+    let theDish = dishesList.find(item => {
+      return item.name == dishes[i].dish_name;
+    });
+
+    theDish.items.push(dishes[i]);
+  }
+
+  return dishesList;
 }
--- routes/food.js
+++ routes/food.js
@@ -8,26 +8,35 @@ const exampleDishesObject = require("../exampleDishes");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  console.log(req.query);
-  res.render("ingredients", exampleIngredientsObject);
-  // db.getUsers()
-  //   .then(users => {
-  //     res.render('index', exampleIngredientsObject)
-  //   })
-  //   .catch(err => {
-  //     res.status(500).send('Well this isnt good :(' + err.message)
-  //   })
+  // res.render("ingredients", exampleIngredientsObject);
+  db.getIngredients()
+    .then(ingredients => {
+      let ingredientList = {
+        categories: [
+          {
+            name: "Alll the stuff",
+            items: ingredients
+          }
+        ]
+      };
+      res.render("ingredients", ingredientList);
+    })
+    .catch(err => {
+      res.status(500).send("Well this isnt good :(" + err.message);
+    });
 });
 
 router.get("/dishes", (req, res) => {
-  res.render("dishes", exampleDishesObject);
-  // db.getDishes(ingredients)
-  //   .then(dishes => {
-  //     res.render('dishes', exampleDishesObject)
-  //   })
-  //   .catch(err => {
-  //     res.status(500).send('I think I dropped a plate' + err.message)
-  //   })
+  // res.render("dishes", exampleDishesObject);
+  db.getAllDishes()
+    .then(dishes => {
+      //let dishesList = db.formatDishes(dishes);
+      //console.log(JSON.stringify(dishesList, null, 2));
+      res.render("dishes", { dishes: dishes });
+    })
+    .catch(err => {
+      res.status(500).send("I think I dropped a plate" + err.message);
+    });
 });
 
 module.exports = router;
